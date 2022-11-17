7c713eeb91e33235084c398f605b6ae4533a41fc
Linadini <mzubanzimajola2@gmail.com>
2022-11-17 20:42:59 +0200

    assignment

--- 218573819_Majola_ML_assignment1.py
+++ 218573819_Majola_ML_assignment1.py
@@ -1,146 +1,81 @@
-## problem 1
+# Majola_ml 218573819
+#problem 1
 
-def list_check(nums):
-    '''
-    check and return true if the sequence 0,1,3
-    appers in the list somewhere
-    '''
-    for i in range(0,len(nums)-1):
-        if nums[i]==0:
-            if nums[i+1]==2:
-                if nums[i+2]==3:
-                    return True
-
-    return False 
-
-nums=[1, 0, 2, 3, 1]
-print(list_check(nums))
+s ="fullstackslp"
 
 
-##problem 2
+# use indexing to print the following
 
-def string_bits(str):
-    '''
-    return a new string made of every other character
-    starting with the first, so "Hello" yeilds at "Hlo"
-    '''
-    newStr =""
-    length = len(str)
-    index = 0
-    while index < length:
-        newStr= newStr+str[index]
-        index =index +2
-        
+#'f'
+print(s[0])
 
-    return newStr
+#'p'
+print(s[-1])
 
-result ="heeololeo"
-print(string_bits(result))
+#'stack'
+print(s[4:9])
 
+#'slp'
+print(s[9:13])
 
-#problem 3
+#'cks'
+print(s[7:10])
 
-def end_other(a,b):
-    '''
-    return true if either of the strings appears at the very end
-    of other string, ignoring the upper/lower case differrences (in other words, 
-    the computation should not be "case sensitive" )
-    '''
-    a1 =a.lower()
-    b1 = b.lower()
+#'Bonus: use indexing to reverse the string'
 
-    if(a1[-1]== b1[-1]):
-        if(a1[-2]== b1[-2]):
-            if(a1[-3]== b1[-3]):
-                return True
-    else:
-        return False
-
-#first testing 
-string1="heeololeo"
-string2='leo'
-print(end_other(string1,string2))
-
-#second testing 
-string11="myname"
-string22='mamama'
-print(end_other(string11,string22))
-
-## problem 4
-
-def double_char(str):
-    '''
-    return a string where for evey char in the original,
-    there are two chars
-    '''
-    
-    i=0
-    length = len(str)
-    result = ""
-    while i < length:
-        result = result + str[i]
-        result = result + str[i]
-        i=i+1
-        
-    return result
-
-string22='mamama'
-print(double_char(string22))
+reversedString=''
+index = len(s)
+while index > 0:
+    reversedString += s[ index - 1 ]
+    index = index - 1
+print(reversedString)
 
 
-## problem 5
-def no_teen_sum(a, b, c):
-    '''
-    return their sum. However, if any of the values 
-    is a teen -- in the range 13..19 inclusive -- then that value counts as 0, 
-    except 15 and 16 do not count as a teens
-    '''
-    sum = fix_teen(a) + fix_teen(b) + fix_teen(c)
-    return sum
-    
-def fix_teen(n):
-    '''
-    takes in an int value and returns that value fixed for the 
-    teen rule. In this way
-    '''
-    if 13 <= n and n <= 19 and n != 15 and n!= 16:
-        return 0
-            
-    return n
+## problem 2 [LISTS]
+
+l =[3,7,[5,[1,4,"hello"]]]
 
 
-# testing 
-q=17
-f=19
-h =13
-print(no_teen_sum(q,f,h))
+## reassigning "hello" to be "goodbye"
+l[2][1][2] = 'goodbye'
+print(l)
 
-# testing2
-q=17
-f=9
-h =3
-print(no_teen_sum(q,f,h))
+## problem 3 Dictionaries
 
+# using keys and indexing, grab the "hello" from the following dictionaries
+d1 = {'simple_key':'hello'}
+d1['simple_key']
+print(d1['simple_key'])
 
-## problem 6
-def count_evens(nums):
-    '''
-    returns the number of even integers in the given array
-    '''
+d2 = {'k1':{'k2':'hello'}}
+d2['k1']['k2']
+print(d2['k1']['k2'])
 
-    even_count = 0
-    x = 0
-    
-      
-    while(x < len(nums)):
-        
-        
-        if nums[x] % 2 == 0:
-            even_count += 1
-    return even_count
+d3 = {'k1':[{'nest_key':['this is deep', ['hello']]}]}
+d3['k1'][0]['nest_key'][1][0]
+print(d3['k1'][0]['nest_key'][1][0])
 
 
-#testing 
-list1 = [10, 21, 4, 45, 66, 93, 11]
 
-print(count_evens(list1))
+###problem 4
+mylist = [1,1,1,1,1,2,2,2,2,3,3,3] 
+
+set_list = set(mylist) 
+print("The unique values of the input list using set()") 
+mylist = (list(set_list))
+ 
+for item in mylist: 
+    print(item) 
+
+
+### problem 5 formatting
+
+age =45
+name = "kyle"
+
+print("hello my dog's name is  {} and he looks {} years old".format(name,age))
+
+
+
+
+
